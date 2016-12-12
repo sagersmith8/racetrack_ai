3e3074e7cd2855f1792530fa16fc8ee4434e7353
bridger.howell <d25w197@bz-cs-cslab16l.msu.montana.edu>
2016-12-11 20:08:03 -0700

    Remove unused imports

--- src/main/java/com.ai/Main.java
+++ src/main/java/com.ai/Main.java
@@ -4,9 +4,6 @@ import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 import java.io.IOException;
 
